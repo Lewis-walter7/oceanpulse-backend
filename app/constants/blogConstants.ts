7c3d4c47a77df6c07a32c17111f3cdb8035e898e
Lewis <lewiswalter192@gmail.com>
2024-01-29 19:50:54 +0300

    Second commit

--- app/constants/blogConstants.ts
+++ app/constants/blogConstants.ts
@@ -3,19 +3,19 @@ import { BlogPost } from "../models/BlogPost"
 export function createBlogPosts(): BlogPost[] {
     const blogPosts: BlogPost[] = [
       new BlogPost(
+        "https://www.ecowatch.com/marine-life-ocean-threats.html",
         "Marine life and ocean threats",
-        "https://cdn-fhofj.nitrocdn.com/YLARnxovRxHnoSTcLUnkvhePKVxPqkls/assets/images/optimized/rev-dfb2699/www.ecowatch.com/wp-content/uploads/2022/10/sea-turtle-101.jpg",
         "Everything you need to know",
-        "https://www.ecowatch.com/marine-life-ocean-threats.html"
+        "https://cdn-fhofj.nitrocdn.com/YLARnxovRxHnoSTcLUnkvhePKVxPqkls/assets/images/optimized/rev-dfb2699/www.ecowatch.com/wp-content/uploads/2022/10/sea-turtle-101.jpg"
     ),
     new BlogPost(
+        "https://www.ecowatch.com/microplastics-galapagos-penguins.html",
         "Accumulation of microplastics",
-        "https://cdn-fhofj.nitrocdn.com/YLARnxovRxHnoSTcLUnkvhePKVxPqkls/assets/images/optimized/rev-dfb2699/www.ecowatch.com/wp-content/uploads/2024/01/galapagos-penguins.jpg",
         "Food Chain of Endangered Galápagos Penguins",
-        "https://www.ecowatch.com/microplastics-galapagos-penguins.html"
+        "https://cdn-fhofj.nitrocdn.com/YLARnxovRxHnoSTcLUnkvhePKVxPqkls/assets/images/optimized/rev-dfb2699/www.ecowatch.com/wp-content/uploads/2024/01/galapagos-penguins.jpg"
     ),
     ];
-  
+
     return blogPosts;
   }
   
